b20748d765c006d6099b7678fea93cbed1ea9930
GervasioEtienot <gervasioe@gmail.com>
2019-08-29 16:39:01 -0300

    Segundo commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
 
-function App() {
+Amplify.configure(awsconfig);
+
+const listTodos = `query listTodos {
+  listTodos{
+    items{
+      
+      name
+      description
+    }
+  }
+}`
+
+const addTodo = `mutation createTodo($name:String! $description: String!) {
+  createTodo(input:{
+    name:$name
+    description:$description
+  }){
+    id
+    name
+    description
+  }
+}`
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      name:"",
+      description:""
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.todoMutation = this.todoMutation.bind(this);
+  }
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value});
+  }
+  
+  todoMutation = async () => {
+    const todoDetails = {
+      name: this.state.name,
+      description: this.state.description
+    };
+    
+    const newTodo = await API.graphql(graphqlOperation(addTodo, todoDetails));
+    alert(JSON.stringify(newTodo));
+  }
+
+  listQuery = async () => {
+    console.log('listing todos');
+    const allTodos = await API.graphql(graphqlOperation(listTodos));
+    alert(JSON.stringify(allTodos));
+    
+  }
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <form>
+         <input type="text" name="name" placeholder="Nombre" value={this.state.name} onChange={this.handleChange} />
+         <input type="text" name="description" placeholder="Descripción" value={this.state.description} onChange={this.handleChange} />
+       </form>
+       <button onClick={this.todoMutation}>GraphQL Mutation</button>
+       <button onClick={this.listQuery}>GraphQL Query</button> 
     </div>
   );
 }
+}
 
-export default App;
+export default withAuthenticator(App, true);
